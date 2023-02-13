2a43b8a1f48b8ab7f5ab8332a51b216324d123e3
Yoo <santiagofrieza@gmail.com>
2023-02-13 13:30:33 -0500

    New component to make fields

--- src/Components/Field.jsx
+++ src/Components/Field.jsx
@@ -1,2 +1,39 @@
+import { useState } from "react";
+
 function Field() {
+  const [Edit_, setEdit] = useState(true);
+  const [text, setText] = useState("");
+
+  const handleInput = (e) => {
+    setText(e.target.value);
+  };
+
+  const Submit = () => {
+    setEdit(false);
+    console.log(this.state.Edit);
+  };
+
+  const Edit = () => {
+    setEdit(true);
+    console.log(this.state.Edit);
+  };
+
+  return (
+    <>
+      {Edit_ && (
+        <form>
+          <input type="text" onChange={handleInput} value={text} />
+          <input type="button" onClick={Submit} value="Save" />
+        </form>
+      )}
+      {Edit_ === false && (
+        <p>
+          {text}
+          <input type="button" onClick={Edit} value="Edit" />
+        </p>
+      )}
+    </>
+  );
 }
+
+export default Field;
